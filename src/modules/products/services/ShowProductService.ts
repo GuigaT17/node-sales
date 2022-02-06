4bd4e3cf859c4e9822f481221d501831b272ece9
GuigaT17 <guigateixeira@protonmail.com>
2022-02-06 16:51:39 -0300

    update product service

--- src/modules/products/services/ShowProductService.ts
+++ src/modules/products/services/ShowProductService.ts
@@ -8,12 +8,12 @@ interface IRequest {
 }
 
 class ShowProductService {
-  public async execute({ id }: IRequest): Promise<Product | undefined> {
+  public async execute({ id }: IRequest): Promise<Product> {
     const productRepository = getCustomRepository(ProductRepository);
 
-    const product = productRepository.findOne(id);
+    const product = await productRepository.findOne(id);
 
-    if (!product){
+    if (!product) {
       throw new AppError('Product not found');
     }
 
