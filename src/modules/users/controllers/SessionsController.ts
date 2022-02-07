363aa2b2202a608c5b9dfa55c2a86357d5f147eb
GuigaT17 <guigateixeira@protonmail.com>
2022-02-07 18:14:27 -0300

    login createsd

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { Session } from 'inspector';
+import CreateSessionService from '../services/CreateSessionSevice';
+
+export default class SessionsController {
+  public async login(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionService();
+
+    const user = await createSession.execute({ email, password });
+
+    return response.json(user);
+  }
+}
