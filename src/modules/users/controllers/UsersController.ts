69b1993783203ac1ca3c0a188638c7b3c4fc0234
GuigaT17 <guigateixeira@protonmail.com>
2022-02-07 15:31:06 -0300

    user controller

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUsersService from '../services/ListUsersService';
+import ShowUserService from '../services/ShowUserService';
+
+export default class UserController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = new ListUsersService();
+
+    const users = await listUsers.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showUser = new ShowUserService();
+
+    const user = await showUser.execute({ id });
+
+    return response.json(user);
+  }
+}
