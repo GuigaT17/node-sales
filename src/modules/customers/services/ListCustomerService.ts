2754d619ed94cb1927e88e3a1ad5735d462b8f56
GuigaT17 <guigateixeira@protonmail.com>
2022-02-22 17:05:14 -0300

    pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,13 +2,24 @@ import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import { CustomerRepository } from '../typeorm/repositories/CustomerRepository';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  public async show(): Promise<Customer[]> {
+  public async show(): Promise<IPaginateCustomer> {
     const customerRepository = getCustomRepository(CustomerRepository);
 
-    const customer = await customerRepository.find();
+    const customer = await customerRepository.createQueryBuilder().paginate();
 
-    return customer;
+    return customer as IPaginateCustomer;
   }
 }
 
